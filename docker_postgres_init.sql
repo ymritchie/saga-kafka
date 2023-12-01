a0de4475ae665131bd6353fdf8c2c8f3fe7b30d0
Yanisley Mora Ritchie <ymritchie@gmail.com>
2023-12-01 07:08:11 -0300

    Tópicos criados e cinctados

---
+++ docker_postgres_init.sql
@@ -0,0 +1,29 @@
+CREATE USER product WITH PASSWORD 'product' CREATEDB;
+CREATE DATABASE product_db
+    WITH
+    OWNER = product
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE USER payment WITH PASSWORD 'payment' CREATEDB;
+CREATE DATABASE payment_db
+    WITH
+    OWNER = payment
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE USER inventory WITH PASSWORD 'inventory' CREATEDB;
+CREATE DATABASE inventory_db
+    WITH
+    OWNER = inventory
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
